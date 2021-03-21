e63ef1591e88dac45219fa2459f72b6140841a04
Minah Yang <lucia.yang@colorado.edu>
2021-03-21 11:56:42 -0600

    generate data and train

--- codes/genData.m
+++ codes/genData.m
@@ -1,29 +1,88 @@
+clear all
+
 %% In this file, we will generate data using the constant weight strategy.
 % We chose this so that the portfolio has no impact on the market.
 % We vary the amplitudes of mu c, and M, and the rank of M. 
-addpath('/projects/luya7574/FM21/MathWorks')
-T = 1000;
-d = 100;
-eta = 0.002;
+addpath('/projects/luya7574/MathWorks')
+addpath('/projects/luya7574/FM21/codes')
+%% Model and Simulator Initialization 
 
-M_amp=5e-3;
-mu_amp=2-5;
-c_amp=1e-8;
+% Initialize Model Parameters
+T = 500;
+d = 50;
+eta = 0.0002;
+trials = 10;
+data = zeros(trials,d,T);
+for t = 1 : trials
+    Mrank = floor(0.25*d);
+    [U,S,V] = svd( randn(d,d) );
+    diagM = diag( [ normrnd(0,1,Mrank,1) ; zeros(d-Mrank,1) ] );
+    M = 5e-3 * U * diagM * V'; % Randomly generated matrix of rank Mrank
 
-Mrank = floor(0.25*d);
-[U,S,V] = svd( randn(d,d) );
-diagM = diag( [ normrnd(0,1,Mrank,1) ; zeros(d-Mrank,1) ] );
-M = M_amp* U * diagM * V'; % Randomly generated matrix of rank Mrank
+    mu = 2e-5 * normrnd(0,1,d,1).^2;
+    c = 1e-8 * normrnd(0,1,d,1).^2;
+    s0 = 100*ones(d,1);
 
-mu = mu_amp * normrnd(0,1,d,1).^2;
-c = c_amp * normrnd(0,1,d,1).^2;
-s0 = 100*ones(d,1);
+    % Initialize Simulation Environment
+    model_params = struct('mu',mu,'M',M,'c',c,'eta',eta);
+    sim_obj = MarketSimulator(T,s0,model_params);
 
-% Initialize Simulation Environment
-model_params = struct('mu',mu,'M',M,'c',c,'eta',eta);
-sim_obj = MarketSimulator(T,s0,model_params);
+    %% Visualization of a Single Simulation for a Strategy
 
-%% Visualization of a Single Simulation for a Strategy
+    % Run strategy on environment
+    sim_obj = do_nothing(sim_obj);
+    data(t,:,:)=sim_obj.s_hist(:,1:end-1);
+end
 
-% Run strategy on environment
-sim_obj = example_strategy_1(sim_obj);
+save('../data/DATA.mat','data')
+numSteps = 40;
+n.TRAIN = floor(0.75*T);
+n.TEST = T-n.TRAIN;
+
+%for t = 1 : trials
+t=1;
+Xtrn = zeros(d,n.TRAIN-numSteps-1,numSteps);
+Ytrn = zeros(d,n.TRAIN-numSteps-1);
+for i = 1 : size(Xtrn,2)
+    Xtrn(:,i,:) = data(t,:,i:i+numSteps-1);
+    Ytrn(:,i) = data(t,:,i+numSteps);
+end
+Xval = zeros(d,n.TEST-numSteps-1,numSteps);
+Yval = zeros(d,n.TEST-numSteps-1);
+for i = 1 : size(Xval,2)
+    Xval(:,i,:) = data(t,:,n.TRAIN+i:n.TRAIN+i+numSteps-1);
+    Yval(:,i) = data(t,:,n.TRAIN+i+numSteps);
+end
+Xtrn = reshape(Xtrn, d*size(Xtrn,2),[]);
+Xval = reshape(Xval, d*size(Xval,2),[]);
+Ytrn = reshape(Ytrn, d*size(Ytrn,2),[]);
+Yval = reshape(Yval, d*size(Yval,2),[]);
+
+Xt = Xtrn;
+Yt = Ytrn;
+Xv = Xval;
+Yv = Yval;
+
+for t = 2 : trials
+    Xtrn = zeros(d,n.TRAIN-numSteps-1,numSteps);
+    Ytrn = zeros(d,n.TRAIN-numSteps-1);
+    for i = 1 : size(Xtrn,2)
+        Xtrn(:,i,:) = data(t,:,i:i+numSteps-1);
+        Ytrn(:,i) = data(t,:,i+numSteps);
+    end
+    Xval = zeros(d,n.TEST-numSteps-1,numSteps);
+    Yval = zeros(d,n.TEST-numSteps-1);
+    for i = 1 : size(Xval,2)
+        Xval(:,i,:) = data(t,:,n.TRAIN+i:n.TRAIN+i+numSteps-1);
+        Yval(:,i) = data(t,:,n.TRAIN+i+numSteps);
+    end
+    Xtrn = reshape(Xtrn, d*size(Xtrn,2),[]);
+    Xval = reshape(Xval, d*size(Xval,2),[]);
+    Ytrn = reshape(Ytrn, d*size(Ytrn,2),[]);
+    Yval = reshape(Yval, d*size(Yval,2),[]);
+    Xt = cat(1,Xt,Xtrn);
+    Yt = cat(1,Yt,Ytrn);
+    Xv = cat(1,Xv,Xval);
+    Yv = cat(1,Yv,Yval);
+end
+clear data Xval Yval Xtrn Ytrn
--- codes/train.m
+++ codes/train.m
@@ -1,19 +1,42 @@
-%% Try just forecasting the next step.
-numFeatures = 1;
-numResponses = 1;
-numHiddenUnits = 200;
+genData
+Xt = Xt';Xv = Xv';
+Xt = reshape(Xt,[numSteps 1 1 size(Xt,2)]);
+Xv = reshape(Xv,[numSteps 1 1 size(Xv,2)]);
 
-layers = [ ...
-    sequenceInputLayer(numFeatures)
-    lstmLayer(numHiddenUnits)
-    fullyConnectedLayer(numResponses)
+modelLG = [
+    imageInputLayer([numSteps 1 1],'Name','input_encoder','Normalization','none')
+    convolution2dLayer([3 1], 3, 'Padding','same', 'Stride', 1, 'Name', 'conv1a')
+    eluLayer('Name','elu1a')
+    convolution2dLayer([3 1], 9, 'Padding','same', 'Stride', 1, 'Name', 'conv1b')
+    eluLayer('Name','elu1b')
+    convolution2dLayer([3 1], 27, 'Padding','same', 'Stride', 1, 'Name', 'conv1c')
+    eluLayer('Name','elu1c')
+    % maxPooling2dLayer([1 2],'Stride',[1 2],'Name','pool1') % Drops to dsize/2
+    % convolution2dLayer([1 3], 27, 'Padding','same', 'Stride', 1, 'Name', 'conv2a')
+    % eluLayer('Name','elu2a')
+    % convolution2dLayer([1 3], 27, 'Padding','same', 'Stride', 1, 'Name', 'conv2b')
+    % eluLayer('Name','elu2b')
+    % maxPooling2dLayer([1 2],'Stride',[1 2],'Name','pool2') % Drops to dsize/4
+    % convolution2dLayer([1 3], 27, 'Padding','same', 'Stride', 1, 'Name', 'conv3a')
+    % eluLayer('Name','elu3a')
+    % convolution2dLayer([1 3], 27, 'Padding','same', 'Stride', 1, 'Name', 'conv3b')
+    % eluLayer('Name','elu3b')
+    fullyConnectedLayer(50, 'Name', 'fc_encoder1')
+    eluLayer('Name','elu2a')
+    fullyConnectedLayer(1, 'Name', 'fc_encoder2')
     regressionLayer];
+%modelNet = dlnetwork(modelLG);
 
-    options = trainingOptions('adam', ...
-    'MaxEpochs',250, ...
-    'GradientThreshold',1, ...
-    'InitialLearnRate',0.005, ...
-    'LearnRateSchedule','piecewise', ...
-    'LearnRateDropPeriod',125, ...
-    'LearnRateDropFactor',0.2, ...
-    'Verbose',1);
+epochs = 10
+options = trainingOptions('adam', ...
+    'InitialLearnRate',3e-4, ...
+    'SquaredGradientDecayFactor',0.99, ...
+    'MaxEpochs',epochs, ...
+    'Shuffle','once',...
+    'MiniBatchSize',1800, ...
+    'ValidationData',{Xv,Yv});
+
+
+net = trainNetwork(Xt,Yt,modelLG,options);
+Yprd = predict(net,Xv);
+mean(abs((Yprd - Yv) ./ Yv))
