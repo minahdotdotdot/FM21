99daeb788e2faf73b6acaa9199d3b01e8f1b9dd3
dk9011 <dannykurban@gmail.com>
2021-04-12 11:34:29 -0600

    plot EF subpltos

---
+++ codes/equal_weights.m
@@ -0,0 +1,11 @@
+function simObj = equal_weights(simObj,lambda)
+    % Equal-weighted porfolio strategy (invariant to lambda)
+    if nargin<2
+        lambda = 0.5;
+    end
+    w_const = ones(simObj.d,1)/simObj.d; % equal weighted portfolio vector
+    simObj = simObj.reset(); % reset simulation environment
+    for i=1:simObj.T
+       simObj = simObj.step(w_const);
+    end
+end
