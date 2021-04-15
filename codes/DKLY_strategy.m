3951078c2fd4b57a9763997bc7edb90d96dac365
Minah Yang <lucia.yang@colorado.edu>
2021-04-14 20:03:44 -0600

    final strategy

--- codes/DKLY_strategy.m
+++ codes/DKLY_strategy.m
@@ -1,7 +1,8 @@
-function simObj =cool_strategy(simObj, lambda,k,lambda_0)
-    % Pick best-performing stocks each period
+function simObj =cool_strategy(simObj, lambda)
+    k = .25;
+    lambda_0=25;
+    % Compute alpha and n.
     [a,n] = mapl2a(lambda, simObj.d, k, lambda_0);
-    b = 1-a;
     if nargin<2
         lambda = 0.5;
     end
@@ -13,10 +14,19 @@ function simObj =cool_strategy(simObj, lambda,k,lambda_0)
         end
         if x>1
             w = zeros(simObj.d,1);
+            % Order stocks by stock prices in descending order.
             [~, sortedInds] = sort(simObj.s_cur(:),'descend');
+
+            % Set winners to alpha/n.
             w(sortedInds(1:n))=a/n;
-            w(sortedInds(n+1:end))=b/(simObj.d-n);
+
+            % Set losers to (1-alpha)/(d-n)
+            w(sortedInds(n+1:end))=(1-a)/(simObj.d-n);
+
+            % Make sure weights add up to 1. 
             w = w / sum(w);
+
+            % Take a step.
             simObj.step(w);
         end          
     end
